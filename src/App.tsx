9e27457d0a2365b683789dce2f8b2114c350b0ee
Oskar <oskar.calvo@aeg.eus>
2025-10-13 12:06:16 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Citadel of Eternal Night</h1>
+      <p>Construye aquí la interfaz y lógica de la ciudadela.</p>
+    </div>
+  )
+}
+
+export default App
